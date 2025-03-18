9e800c2a1e9c881955253df58b0d8ad03e190bdb
brill1802 <patvarit.k@mandarin.ac.th.com>
2025-03-18 12:37:47 +0700

    add folder

---
+++ cell01/ex03/what_is_your_name.py
@@ -0,0 +1,3 @@
+x = input("my first_name is ")
+y = input("my last name is ")
+print(x+" "+y)
--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,5 +1,4 @@
-x = 0 or 1
-if x==0:
-    print ("this number is zero")
-if x==1:
-    print ("this number is not zero")
+x=int(input("insert a number"))
+if x == 0 :print ("this number is zero")
+elif x > 0 :print ("this number larger than zero")
+else :print("this number smaller than zero")
