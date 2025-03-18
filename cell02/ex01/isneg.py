4b0b4b9a1baa3accb0ceecf828242f5832f91146
brill1802 <patvarit.k@mandarin.ac.th.com>
2025-03-18 14:32:45 +0700

    add folder

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,4 +1,4 @@
-x = int(input("pick a number"))
+x = int(input("pick a number "))
 if x < 0 :print ("it's a negative number")
 elif x > 0 :print ("it's a positive number")
 else :print ("it's both negative and positive")
---
+++ cell02/ex03/password.py
@@ -0,0 +1,4 @@
+password = "pikachu"
+x = input("password:")
+if x == "pikachu":print ("correct password")
+else :print ("wrong password")
